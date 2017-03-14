431cd79d589bf191eaeec9cbbc09ff227ed7b1d8
Simon Crowley <crowleysimondev@gmail.com>
2017-03-15 08:05:07 +1000

    Add recipe data model

--- app/src/main/java/com/crowleysimon/basil/model/Recipe.java
+++ app/src/main/java/com/crowleysimon/basil/model/Recipe.java
@@ -1,4 +1,54 @@
 package com.crowleysimon.basil.model;
 
-public class Recipe {
+import java.util.Date;
+
+import io.realm.RealmObject;
+
+public class Recipe extends RealmObject {
+
+    private Date savedDate;
+    private String url;
+    private String title;
+    private String description;
+    private String imageUrl;
+
+    public Date getSavedDate() {
+        return savedDate;
+    }
+
+    public void setSavedDate(Date savedDate) {
+        this.savedDate = savedDate;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
---
+++ app/src/main/java/com/crowleysimon/basil/util/RecipeUtil.java
@@ -0,0 +1,24 @@
+package com.crowleysimon.basil.util;
+
+import android.support.annotation.NonNull;
+
+import com.crowleysimon.basil.model.Recipe;
+import com.schinizer.rxunfurl.model.PreviewData;
+
+import java.util.Calendar;
+
+public class RecipeUtil {
+
+    public static Recipe createRecipeFromPreview(@NonNull PreviewData previewData) {
+        Recipe recipe = new Recipe();
+        recipe.setDescription(previewData.getDescription());
+        if (previewData.getImages() != null && !previewData.getImages().isEmpty()) {
+            recipe.setImageUrl(previewData.getImages().get(0).getSource());
+        }
+        recipe.setTitle(previewData.getTitle());
+        recipe.setUrl(previewData.getUrl());
+        Calendar calendar = Calendar.getInstance();
+        recipe.setSavedDate(calendar.getTime());
+        return recipe;
+    }
+}
