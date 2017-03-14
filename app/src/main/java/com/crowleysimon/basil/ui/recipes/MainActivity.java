997d7c57b0b16c6b071552d1b9954248583a9456
Simon Crowley <crowleysimondev@gmail.com>
2017-03-15 08:41:05 +1000

    refactored existing code to extend base classes

--- app/src/main/java/com/crowleysimon/basil/ui/recipes/MainActivity.java
+++ app/src/main/java/com/crowleysimon/basil/ui/recipes/MainActivity.java
@@ -1,54 +1,21 @@
 package com.crowleysimon.basil.ui.recipes;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.crowleysimon.basil.R;
+import com.crowleysimon.basil.ui.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.common_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (savedInstanceState == null) {
+            setFragment(MainActivityFragment.class);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/crowleysimon/basil/ui/recipes/MainActivityFragment.java
+++ app/src/main/java/com/crowleysimon/basil/ui/recipes/MainActivityFragment.java
@@ -1,17 +1,18 @@
 package com.crowleysimon.basil.ui.recipes;
 
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.crowleysimon.basil.R;
+import com.crowleysimon.basil.ui.addrecipe.AddRecipeActivity;
+import com.crowleysimon.basil.ui.base.BaseFragment;
 
-/**
- * A placeholder fragment containing a simple view.
- */
-public class MainActivityFragment extends Fragment {
+import butterknife.OnClick;
+
+public class MainActivityFragment extends BaseFragment {
 
     public MainActivityFragment() {
     }
@@ -20,5 +21,10 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @OnClick(R.id.fab)
+    public void onFabCLick() {
+        startActivity(new Intent(getActivity(), AddRecipeActivity.class));
     }
 }
